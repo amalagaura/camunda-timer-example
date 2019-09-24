ff3b2a33b6a3ad9ad5cfd72cb93ba9b5b98a6446
Ankur Sethi <ankur.sethi@uscis.dhs.gov>
2019-09-24 10:27:47 -0400

    Change to user task

--- src/test/java/example/ProcessUnitTest.java
+++ src/test/java/example/ProcessUnitTest.java
@@ -1,10 +1,12 @@
 package example;
 
 import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.impl.util.ClockUtil;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.RequiredHistoryLevel;
 import org.camunda.bpm.spring.boot.starter.test.helper.StandaloneInMemoryTestConfiguration;
 import org.joda.time.DateTime;
 import org.junit.Rule;
@@ -36,23 +38,25 @@ public class ProcessUnitTest {
     public final ProcessEngineRule processEngine = new StandaloneInMemoryTestConfiguration().rule();
 
     @Test
+    @RequiredHistoryLevel(ProcessEngineConfiguration.HISTORY_FULL)
     @Deployment(resources = BPMN_RESOURCE)
     public void testHappyPath() {
         // Either: Drive the process by API and assert correct behavior by camunda-bpm-assert, e.g.:
         ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
 
         // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
-        assertThat(processInstance).isWaitingAtExactly("WaitHere");
-        complete(externalTask());
+        assertThat(processInstance).isWaitingAtExactly("UserTask");
+        execute(job());
         assertThat(processInstance).isEnded();
     }
 
     @Test
+    @RequiredHistoryLevel(ProcessEngineConfiguration.HISTORY_FULL)
     @Deployment(resources = BPMN_RESOURCE)
     public void testWaitState() {
         ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
-        assertThat(processInstance).isWaitingAtExactly("WaitHere");
+        assertThat(processInstance).isWaitingAtExactly("UserTask");
         ClockUtil.setCurrentTime(new DateTime().plusWeeks(2).toDate());
-        assertThat(processInstance).isWaitingAtExactly("WaitHere", "MessageEndEvent");
+        assertThat(processInstance).isWaitingAtExactly("UserTask", "MessageEndEvent");
     }
 }
